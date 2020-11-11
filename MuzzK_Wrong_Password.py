ad1eda98f0a986be4c8319714754ae5cea8c9cdd
MueezAllie <43094130+MueezAllie@users.noreply.github.com>
2020-11-11 15:58:24 +0200

    Add files via upload

---
+++ MuzzK_Wrong_Password.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as paho
+import time
+import random
+import json
+
+def is_alert():
+    
+    n = random.random()
+    print(n)
+    if (n<0.5):
+        return True
+    else:
+        return False
+
+def on_connect(client, userdata, flags, rc):
+    print("CONNACK recieved with code %d." % (rc))
+    
+
+def on_publish(client, userdata, mid):
+    print("mid: " + str(mid))
+    
+
+payload = {'username' : 'MuzzK_CEB_RondeboschEast',
+           'type' : 'Critical Emergency Button'}
+
+
+client = paho.Client()
+client.username_pw_set("MuzzK_CEB_RondeboschEast","password")
+client.on_publish = on_publish
+client.connect("localhost",1883)
+client.loop_start()
+
+while True:
+    alert = is_alert()
+    payload["alert"] = alert
+    print(payload)
+    (rc,mid) = client.publish("alert", json.dumps(payload), qos=0)
+    #print(alert)
+    time.sleep(3)
